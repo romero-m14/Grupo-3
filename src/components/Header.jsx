feaf809e373aec32238e4d0bfc784c7dec7309f8
Aldair Rutte <xxpiltisxx@gmail.com>
2026-06-24 02:19:17 -0500

    Se agregó footer y header

---
+++ src/components/Header.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react"
+
+export default function Header() {
+    const [show, setShow] = useState(false);
+
+    return (
+        <header className="relative flex flex-col gap-4 justify-between bg-navbar text-fondo 
+                           p-6">
+
+            {/* Logo principal */}                
+            <img className="rounded-full bg-fondo w-13" src="/images/logoTransparent.png" alt="Logo de la pagina principal" />
+            
+            {/*  Botón Hamburger */}
+            <button onClick={() => setShow(!show)} 
+                    className="cursor-pointer absolute top-8 right-6 md:hidden flex flex-col gap-2"
+            >
+                <span className={`bg-fondo w-8 h-1 rounded-full transition-all duration-300 ease-in-out ${show ? "opacity-0" : ""}`}></span>
+                <span className={`bg-fondo w-8 h-1 rounded-full transition-all duration-300 ease-in-out ${show ? "rotate-45" : ""}`}></span>
+                <span className={`bg-fondo w-8 h-1 rounded-full transition-all duration-300 ease-in-out ${show ? "-rotate-45 -translate-y-2.5": ""}`}></span>
+            </button>
+
+            {/*  Barra de navegación */}
+            <nav className={`${show ? "flex" : "hidden"} flex-col items-center gap-4 text-base`}>
+                <a className="hover:bg-boton px-20 py-3" href="#">Recipes</a>
+                <a className="hover:bg-boton px-20 py-3" href="#">Collections</a>
+                <a className="hover:bg-boton px-20 py-3" href="#">Techniques</a>
+                <a className="hover:bg-boton px-20 py-3" href="#">Journal</a>
+            </nav>
+        </header>
+    )
+}
